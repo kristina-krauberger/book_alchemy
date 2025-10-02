556667326fcc11ca9f35583e9076025157dc2f66
Kristina Krauberger <krauberger.kristina@gmail.com>
2025-10-02 17:16:16 +0200

    feat: added connection to database and instantiated Flask app.

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
-from flask import Flask,
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from data_models import db, Author, Book
+import os
+
+app = Flask(__name__) # 1. create the app
+
+basedir = os.path.abspath(os.path.dirname(__file__)) # 2. absoluter pfad
+#3. flask mitteilen wo die datenbankdatei ist, absoluter pfad string!
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
+
+db.init_app(app) # 4. initialize the app with the extension
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  with app.app_context(): # Google "How to run SQLight flask app" - wichtig, damit db.create_all() funktioniert
+    db.create_all()        # erzeugt Tabellen, falls noch nicht vorhanden
+
+  app.run(host="0.0.0.0", port=5001, debug=True) # Google "How to run flask app" - Startet server
 
---
+++ data_models.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy #kein Column
+
+db = SQLAlchemy()  # instanziierung des objekts ORM system für Kommunikation mit Datenbank, damit kann ich die Objekte erstellen
+
+class Author(db.Model):
+    __tablename__ = "authors"
+
+    id = db.Column(db.INTEGER, primary_key=True, autoincrement=True)
+    name = db.Column(db.STRING(50))
+    birth_date = db.Column(db.DATE)
+    date_of_death = db.Column(db.DATE)
+
+
+class Book(db.Model):
+    __tablename__ = "books"
+
+    id = db.Column(db.INTEGER, primary_key=True, autoincrement=True)
+    isbn = db.Column(db.STRING(17)) #mit strichen
+    title = db.Column(db.STRING(32)) #binäre schwellenwerte
+    publication_year = db.Column(db.INTEGER)
+
+
